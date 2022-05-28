8d0864a2f36f0d28a4eb9b91af2dcd44fe2a6181
White-Mouse hackr <64909897+glitchish@users.noreply.github.com>
2022-05-28 15:43:12 +0000

    Added some Config files, change some format, yadda yadda.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from tkinter import *
 from tkinter import ttk
 import Pmw
-
+import os
 """Setting Up the OS"""
 nuos = Tk()
 Pmw.initialise(nuos)
@@ -10,19 +10,34 @@ nuos.geometry("1920x1080")
 nuos.title('Nu OS')
 
 Label(nuos,
-text ="Welcome to NuOS Alpha! Written in Python 3 Tkinter by Glitchish").pack()
+      text="Welcome to NuOS Alpha! Written in Python 3 Tkinter by Glitchish"
+      ).pack()
+
 
 def close_nuos():
-  nuos.destroy()
-  time.sleep(1)
-  return 0
-close=Button(nuos, text="×", command=close_nuos, height=1, width=1, bd="0.5px")
+    nuos.destroy()
+    exit("NuOS was Shut-Off")  # This may not work, IDK Y
+
+
+close = Button(nuos,
+               text="×",
+               command=close_nuos,
+               height=1,
+               width=1,
+               bd="0.5px")
 
 close_nuos_tooltip = Pmw.Balloon(close)
-close_nuos_tooltip.bind(close,'Shut-Off NuOS')
-# nuos.bind("<Escape>",close_nuos)
-close.place(rely=1.0, relx=1.0, x=0, y=0, anchor=SE)
+close_nuos_tooltip.bind(close,
+                        'Shut-Off NuOS')
 
-nuos.attributes('-fullscreen',True)
+close.place(rely=1.0,
+            relx=1.0,
+            x=0,
+            y=0,
+            anchor=SE)
+
+nuos.attributes('-fullscreen', True)
 
 mainloop()
+"""Clear the console of unnecessary errors"""
+os.system("cls||clear")
